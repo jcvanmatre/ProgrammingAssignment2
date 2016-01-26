7e25cd08b7f78cdc35657fe8bc1ee11a1e276842
jcvanmatre <jcvanmatre@gmail.com>
2016-01-26 08:13:13 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This fuction is for matrix inversion. The first fxn
+## creates a list sets the parameters, and the second
+## function creates checks if the matrix has already
+## been caclucalted and then returns either the 
+## cacluaton or the cached calculation. 
 
-## Write a short comment describing this function
+## This first fuction creates a list that:
+## 1.  sets the value of the vector
+## 2.  gets the value of the vector
+## 3.  sets the value of the mean
+## 4.  gets the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  LeInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    LeInverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) LeInverse <<- inverse
+  getinverse <- function() LeInverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This fxn returns the inverse. First it checks
+## to see if the inverse in cahched, and if it is 
+## it returns it, if not it caluclates it and 
+## returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  LeInverse <- x$getinverse()
+   if(!is.null(LeInverse)) {
+    message("getting cached inverse")
+    return(LeInverse)
+  }
+  data <- x$get()
+  LeInverse <- solve(data)
+  x$setinverse(LeInverse)
+  LeInverse
 }
+  
+## Return a matrix that is the inverse of 'x'
+SampleMX <- matrix(1:4,2,2)
+MX = makeCacheMatrix(SampleMX)
+cacheSolve(MX) ##first itt
+cacheSolve(MX) ##second itt with message
